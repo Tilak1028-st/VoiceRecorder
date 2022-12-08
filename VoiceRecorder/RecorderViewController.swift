27f26525ed41ff4d22fa6963a4162a3851667083
Shakya Tilak <shakyatilak45@gmail.com>
2022-12-08 17:13:52 +0530

    Added TableView For Record List

--- VoiceRecorder/RecorderViewController.swift
+++ VoiceRecorder/RecorderViewController.swift
@@ -12,106 +12,92 @@ class RecorderViewController: UIViewController {
 
     @IBOutlet weak var recorderView: UIView!
     @IBOutlet weak var micButton: UIButton!
-    
-    var captureSession: AVCaptureSession?
-    var audioDevice: AVCaptureDevice?
-    var audioDeviceInput: AVCaptureDeviceInput?
-    var audioDeviceOutput: AVCaptureAudioDataOutput?
+    @IBOutlet weak var recordListTableView: UITableView!
+    var recordingSession: AVAudioSession!
+    var audioRecoder: AVAudioRecorder!
+    var numberOfRecords = 0
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.styleMicButton()
-        prepare { (error) in
-            if let error = error {
-                print(error)
-            }
+        recordingSession = AVAudioSession.sharedInstance()
+        if let number:Int = UserDefaults.standard.object(forKey: "Mynumber") as? Int {
+            numberOfRecords = number
         }
+        checkMicPremission()
+        self.styleMicButton()
     }
 
     
-    func styleMicButton() {
-        micButton.layer.cornerRadius = min(micButton.frame.width, micButton.frame.height) / 2
-    }
     
     @IBAction func diTapOnRecordButton(_ sender: Any) {
        
+        if audioRecoder == nil {
+            let fileName = getFileDirectory()?.appendingPathComponent("\(numberOfRecords).m4a")
+            
+            let setting = [AVFormatIDKey: Int(kAudioFormatMPEG4AAC), AVSampleRateKey: 1200, AVNumberOfChannelsKey: 1, AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue]
+            
+            do {
+                audioRecoder = try AVAudioRecorder(url: fileName!, settings: setting)
+                audioRecoder.delegate = self
+                audioRecoder.record()
+            }
+            catch {
+                print(error.localizedDescription)
+            }
+        }
+        else {
+            audioRecoder.stop()
+            audioRecoder = nil
+            
+            UserDefaults.standard.set(numberOfRecords, forKey: "Mynumber")
+            recordListTableView.reloadData()
+        }
     }
     
 }
 
 extension RecorderViewController {
-    func prepare(completionHandler: @escaping (Error?) -> Void) {
-        
-        func createCaptureSession() {
-            captureSession = AVCaptureSession()
-        }
-        
-        func configureCaptureDevice() throws {
-            guard let capturedAudioDevice = AVCaptureDevice.default(for: .audio) else {
-                throw RecorderError.captureDeviceIsNotAvailable
-            }
-            
-            self.audioDevice = capturedAudioDevice
-        }
-        
-        func configureDeviceInput() throws {
-            guard let captureSession = captureSession else {
-                throw RecorderError.captureSessionIsMissing
-            }
-            
-            if let audioDevice = self.audioDevice {
-                try audioDevice.lockForConfiguration()
-                self.audioDeviceInput = try AVCaptureDeviceInput.init(device: audioDevice)
-                try audioDevice.unlockForConfiguration()
-                
-                if captureSession.canAddInput(self.audioDeviceInput!) {
-                    captureSession.addInput(audioDeviceInput!)
-                }
-            }
-        }
-        
-        func configureDevicOutput() throws {
-            guard let captureSession = captureSession else {
-                throw RecorderError.captureSessionIsMissing
-            }
-            
-            self.audioDeviceOutput = AVCaptureAudioDataOutput()
-            
-            if captureSession.canAddOutput(self.audioDeviceOutput!) {
-                captureSession.addOutput(self.audioDeviceOutput!)
-            }
-            captureSession.startRunning()
-        }
-        
-        DispatchQueue(label: "prepare").async {
-            do {
-                createCaptureSession()
-                try configureCaptureDevice()
-                try configureDeviceInput()
-                try configureDevicOutput()
-            }
-            catch {
-                DispatchQueue.main.async {
-                    completionHandler(error)
-                }
-                
-                return
-            }
-            DispatchQueue.main.async {
-                completionHandler(nil)
-            }
-        }
-    
+
+//MARK: -> Function to make mic button circle
+    func styleMicButton() {
+        micButton.layer.cornerRadius = min(micButton.frame.width, micButton.frame.height) / 2
     }
+    
+//MARK: -> Function to check Mic Premission
+    func checkMicPremission() {
+        AVAudioSession.sharedInstance().requestRecordPermission { (hasPermission) in
+            if hasPermission {
+                print("Having Record Permission")
+            }
+        }
+    }
+    
+//MARK: -> Function to get file Path
+    func getFileDirectory() -> URL? {
+        guard let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            return nil
+        }
+        return path
+    }
+}
+
+// MARK: -> Recorder Delegate method
+extension RecorderViewController: AVAudioRecorderDelegate {
     
 }
 
 
-
-enum RecorderError: Swift.Error {
-    case captureDeviceIsNotAvailable
-    case captureSessionIsMissing
-    case inputsAreInvalid
-    case invalidOperation
-    case unknown
+extension RecorderViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numberOfRecords
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = recordListTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = String(indexPath.row + 1)
+        return cell
+    }
+    
+    
 }
